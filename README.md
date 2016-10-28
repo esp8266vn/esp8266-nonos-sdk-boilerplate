4ddb660990ce2b91354fa34db9d760c320470490
Tuan PM <tuanpm@live.com>
2016-10-28 18:05:05 +0700

    Initial project

---
+++ README.md
@@ -0,0 +1,35 @@
+# ESP8266 NonOS Application sample
+
+## Usage (Check detail from Makefile)
+
+- OTA: Select from Makefile
+```
+OUTPUT_TYPE=combined/ota
+```
+- Base directory for the compiler. Needs a / at the end; if not set it'll use the tools that are in the PATH. Modify `XTENSA_TOOLS_ROOT`
+- FLASH Selection:
+```
+#SPI flash size, in K
+ESP_SPI_FLASH_SIZE_K=1024
+#0: QIO, 1: QOUT, 2: DIO, 3: DOUT
+ESP_FLASH_MODE=0
+#0: 40MHz, 1: 26MHz, 2: 20MHz, 15: 80MHz
+ESP_FLASH_FREQ_DIV=0
+```
+
+## Add module (check module folder user for sample)
+
+```
+MODULES   = folderA folderB
+```
+
+## Compile 
+```
+make  
+make flash 
+make clean
+```
+
+### OTA Flow
+
+![OTA Flow](./assets/fota-flow.png)
---
+++ user/user_main.c
@@ -0,0 +1,35 @@
+
+#include "osapi.h"
+#include "user_interface.h"
+
+void ICACHE_FLASH_ATTR print_info()
+{
+  os_printf("\r\n\r\n[INFO] BOOTUP...\r\n");
+  os_printf("[INFO] SDK: %s\r\n", system_get_sdk_version());
+  os_printf("[INFO] Chip ID: %08X\r\n", system_get_chip_id());
+  os_printf("[INFO] Memory info:\r\n");
+  system_print_meminfo();
+
+  os_printf("[INFO] -------------------------------------------\n");
+  os_printf("[INFO] Build time: %s\n", BUID_TIME);
+  os_printf("[INFO] -------------------------------------------\n");
+
+}
+
+
+void ICACHE_FLASH_ATTR app_init()
+{
+
+
+  print_info();
+
+
+  wifi_set_opmode_current(STATION_MODE);
+
+}
+
+void ICACHE_FLASH_ATTR user_init(void)
+{
+  system_init_done_cb(app_init);
+
+}
